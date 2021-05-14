342f7a11602cdaf672a13aee299314f2e257ac25
lixu <291332445@qq.com>
2021-05-14 23:34:49 +0800

    11

---
+++ src/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'api': '@/api',
+        'assets': '@/assets',
+        'components': '@/components',
+        'views': '@/views',
+      }
+    }
+  }
+}
+
